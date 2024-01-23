e88dd9b5dc6ef793cb230c61afb67a600e749339
Julia <julia.green@dillards.com>
2024-01-22 21:15:35 -0600

    Fixed bug with code

--- react-code-examples.jsx
+++ react-code-examples.jsx
@@ -17,34 +17,13 @@
 
 
 // What JSX Compiles To
-    const element = React.createElement(
-        "nav",
-        { id: "main-nav", className: "navigation"},
-
-        React.createElement(
-            "ul",
-            null,
-
-            React.createElement(
-                "li",
-                null,
-
-                React.createElement(
-                "a",
-                { href: "/" },
-                "Home"
-                )
+    const element = React.createElement( "nav", {id: "main-nav", className: "navigation"},
+        React.createElement("ul", null,
+            React.createElement("li", null,
+                React.createElement("a", {href: "/"}, "Home")
             ),
-
-            React.createElement(
-                "li",
-                null,
-
-                React.createElement(
-                "a",
-                { href: "/archives" },
-                "Archives"
-                )
+            React.createElement("li", null,
+                React.createElement("a", {href: "/archives"}, "Archives")
             )
         )
     );
@@ -52,22 +31,6 @@
 
 
 // Basic Components
-    // Class-Based
-        class Person extends Component {
-            render() {
-            return <h2>Hello world!</h2>;
-            }
-        }
-
-
-    // Functional
-        function Person() {
-            return <h2>Hello world!</h2>;
-        }
-
-
-
-// Rendering Components 
     // Class-Based
         class Person extends Component {
             render() {
@@ -98,6 +61,10 @@
         }
 
         class Daniel extends Person {
+            constructor(props) {
+                super(props); 
+            }
+
             render() {
                 return <h2>Hello, {this.props.name}</h2>;
             }
@@ -109,7 +76,7 @@
             return <Daniel name={"Daniel."} />;
         }
 
-        function Daniel() {
+        function Daniel(name) {
             return <h2>Hello, {props.name}</h2>;
         }
 
@@ -165,15 +132,15 @@
         componentDidMount() {
             console.log("I'm here!");
         }
-    
+
         componentDidUpdate() {
             console.log("Updated!");
         }
-    
+
         componentWillUnmount() {
             console.log("Okay, byeee!");
         }
-    
+
         render() {
             return <h2>Hello world!</h2>;
         }
